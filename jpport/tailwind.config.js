5583a5ae9a6b0e7aa6780b1553251334f173b202
Michael Mount <mount.photos@gmail.com>
2023-08-29 18:25:24 -0600

    Imported Tailwind, daisyUi, and theme changer. Set up tailwind.config file and custom theme

---
+++ jpport/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f0abfc",
+
+          secondary: "#9ca3af",
+
+          accent: "#f5d0fe",
+
+          neutral: "#d1d5db",
+
+          "base-100": "#d6d3d1",
+
+          info: "#f0abfc",
+
+          success: "#f0abfc",
+
+          warning: "#fce7f3",
+
+          error: "#fce7f3",
+        },
+      },
+    ],
+  },
+};
