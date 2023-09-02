e8f3a98c6cf5a378d310e7699a39804be9150896
Michael Mount <mount.photos@gmail.com>
2023-09-02 12:41:47 -0600

    fixed visual bug on mobile navbar of social icons stacking over contact me button, used daisyUI dropdown compontent

--- jpport/src/components/NavBar.js
+++ jpport/src/components/NavBar.js
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
-import { FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa6";
+import { FaInstagram, FaTiktok, FaTwitter, FaBars, FaTimes } from "react-icons/fa";
 
 export default function NavBar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prevState => !prevState);
+  };
+
   return (
     <>
       <div className="navbar bg-secondary">
         <div className="flex-1">
-          <Link to="/" className="btn btn-ghost normal-case text-5xl font-custom text-primary">
+          <Link to="/" className="btn btn-ghost normal-case text-4xl lg:text-5xl font-custom text-primary">
             Jacob Paulson
           </Link>
         </div>
 
         <div className="flex-none">
-          <div className="flex mx-3 ">
+          {/* Desktop Version - Always Display */}
+          <div className="flex mx-3 hidden md:flex"> 
             <a
               href="https://www.instagram.com/_jacobpaulson_/"
               target="_blank"
@@ -38,6 +46,44 @@ export default function NavBar() {
               <FaTwitter />
             </a>
           </div>
+
+          {/* Mobile Version - Dropdown Menu */}
+          <div className="dropdown dropdown-end md:hidden mx-4">
+            <button onClick={toggleMenu} className="btn">
+              {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+            
+            <ul tabIndex="0" className={`p-2 shadow menu dropdown-content bg-secondary text-primary ${isMenuOpen ? 'block' : 'hidden'}`}>
+              <li>
+                <a
+                  href="https://www.instagram.com/_jacobpaulson_/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaInstagram />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.tiktok.com/@jacobryanpaulson"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTiktok />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/JRPaulyStreams"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTwitter />
+                </a>
+              </li>
+            </ul>
+          </div>
+
           <Link to="/Contact" className="btn btn-outline btn-primary lg:mr-6 ">
             Contact Me!
           </Link>
